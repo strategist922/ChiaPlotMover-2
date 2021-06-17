8e1f950367b9287a2586fda5a290060a164641ea
jacoblroberts@gmail.com <jacoblroberts@gmail.com>
2021-06-17 14:44:32 -0400

    asdf

--- Program.cs
+++ Program.cs
@@ -28,101 +28,79 @@ namespace ChiaPlotMover
 
             var startTime = DateTime.Now;
             var totalFilesTransfered = 0;
-            // var directoryInfo = new DriveInfo("/chia/plots/share/208");
-            // Console.WriteLine(directoryInfo.AvailableFreeSpace);
-            //
-            // File.Move("/chia/plottemp2/plot-k32-2021-06-03-17-18-9480cd2c62bcd8f1f81218592995ffe99334185e3cb2ca0947d2a29b6b451789.plot.2.tmp", "/chia/plots/share/208/plot-k32-2021-06-03-17-18-9480cd2c62bcd8f1f81218592995ffe99334185e3cb2ca0947d2a29b6b451789.plot", true);
-            // var plotTempFolders = new List<string> { "/chia/plottemp1", "/chia/plottemp2", "/chia/plottemp3", "/chia/plottemp4"};
-            Console.WriteLine($"Start time - {DateTime.Now}");
+            long totalBytesTransfered = 0;
             var destinationIndex = 0;
+            var maxParallelTransfers = 2;
             while(true) 
             {
-            // var destination = "/chia/plots/share/208";
-            // var destinations = new List<string>() 
-            //     { 
-            //         // "/chia/plots/203",
-            //         // "/chia/plots/204",
-            //         // "/chia/plots/206",
-            //         // "/chia/plots/207",
-            //         "/chia/plots/share/100",
-            //         "/chia/plots/share/101",
-            //         "/chia/plots/share/102",
-            //         "/chia/plots/share/103",
-            //         "/chia/plots/share/104",
-            //         "/chia/plots/share/105",
-            //         "/chia/plots/share/106",
-            //         "/chia/plots/share/107",
-            //         "/chia/plots/share/108",
-            //         "/chia/plots/share/109",
-            //         "/chia/plots/share/200",
-            //         "/chia/plots/share/201",
-            //         "/chia/plots/share/202",
-            //         "/chia/plots/share/203",
-            //         "/chia/plots/share/204",
-            //         "/chia/plots/share/205",
-            //         "/chia/plots/share/206",
-            //         "/chia/plots/share/207",
-            //         "/chia/plots/share/208",
-            //         "/chia/plots/share/209"
-            //     };
-            var maxParallelTransfers = 4;
-            var tasks = new List<Task>();
-            foreach (var folder in config.PlotLocations) 
-            {
-                var plots = Directory.GetFiles(folder, "*.plot").Where(f => f.IndexOf(".plot.") == -1);
-
-                foreach(var plot in plots) 
+                var tasks = new List<Task>();
+                foreach (var folder in config.PlotLocations) 
                 {
-                    if (tasks.Count >= maxParallelTransfers) {
-                        await Task.WhenAll(tasks.ToArray());
-                        tasks.Clear();
-                    }
-                    var fileName = Path.GetFileName(plot);
-                    
-                    var fileInfo = new FileInfo(plot);
-                    while(true)
-                    {
-                        if (destinationIndex == config.DestinationDrives.Count) {
-                            destinationIndex = 0;
-                        }
-                        var destination = config.DestinationDrives[destinationIndex];
-                        destinationIndex++;
-                        var driveInfo = new DriveInfo(destination);
+                    var plots = Directory.GetFiles(folder, "*.plot").Where(f => f.IndexOf(".plot.") == -1);
 
-                        try
-                        {
-                            if (driveInfo.AvailableFreeSpace > fileInfo.Length) 
-                            {
-                                tasks.Add(Task.Run(() => {
-                                    try
-                                    {
-                                        
-                                        Console.WriteLine($"Moving {plot} to {destination}");
-                                        File.Move(plot, Path.Combine(destination, fileName), true);
-                                        Console.WriteLine("Done moving plot... moving next plot");
-                                        totalFilesTransfered++;
-                                    }
-                                    catch(Exception ex)
-                                    {
-                                        Console.WriteLine($"Skipping plot: {fileName}.  Exception: {ex.Message}");
-                                    }
-                                }));
-                                
-                                break;
-                            }
+                    foreach(var plot in plots) 
+                    {
+                        if (tasks.Count >= maxParallelTransfers) {
+                            await Task.WhenAll(tasks.ToArray());
+                            tasks.Clear();
                         }
-                        catch(Exception ex) 
+                        var fileName = Path.GetFileName(plot);
+                        
+                        var fileInfo = new FileInfo(plot);
+                        while(true)
                         {
-                            Console.WriteLine($"Ex: {ex.Message}");
+                            if (destinationIndex == config.DestinationDrives.Count) {
+                                destinationIndex = 0;
+                            }
+                            var destination = config.DestinationDrives[destinationIndex];
+                            destinationIndex++;
+                            var driveInfo = new DriveInfo(destination);
+
+                            try
+                            {
+                                if (driveInfo.AvailableFreeSpace > fileInfo.Length) 
+                                {
+                                    tasks.Add(Task.Run(() => {
+                                        try
+                                        {
+                                            
+                                            Console.WriteLine($"Moving {plot} to {destination}");
+                                            var fileSize = fileInfo.Length;
+                                            File.Move(plot, Path.Combine(destination, fileName), true);
+                                            Console.WriteLine("Done moving plot... moving next plot");
+                                            totalFilesTransfered++;
+                                            totalBytesTransfered += fileSize;
+                                        }
+                                        catch(Exception ex)
+                                        {
+                                            Console.WriteLine($"{DateTime.Now.ToString()}");
+                                            Console.WriteLine($"Exception Dump: {ex}");
+                                            Console.WriteLine($"InnerException: {ex.InnerException}");
+                                        }
+                                    }));
+                                    
+                                    break;
+                                }
+                            }
+                            catch(Exception ex) 
+                            {
+                                Console.WriteLine($"Unexpected Exception: {ex.Message}");
+                                Console.WriteLine($"Stack Trace: {ex.StackTrace}");
+                            }
                         }
                     }
                 }
-            }
-            await Task.WhenAll(tasks.ToArray());
-            tasks.Clear();
-            Console.WriteLine($"Transfered {totalFilesTransfered} since {startTime.ToString()}");
-            Console.WriteLine("Holding 5 minutes...");
-            System.Threading.Thread.Sleep(5 * 60 * 1000);
+                if (tasks.Count > 0) 
+                {
+                    await Task.WhenAll(tasks.ToArray());
+                    tasks.Clear();
+                    Console.WriteLine($"Transfered {totalFilesTransfered} since {startTime.ToString()}");
+                    var diff = DateTime.Now.Subtract(startTime).TotalHours;
+                    Console.WriteLine($"{(totalBytesTransfered / diff) / 1000000000000} TiB/hour");
+                }
+            
+                Console.WriteLine("Holding 2 minutes...");
+                System.Threading.Thread.Sleep(2 * 60 * 1000);
             }
         }
     }
